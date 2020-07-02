1a8a05c8739ddb8df5b630dd64f71a9e295d03c3
MichaelCTH <tonghaoch@qq.com>
2020-07-02 15:36:58 +0800

    use Lato font

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Lato',
       ),
       home: Container(
         decoration: BoxDecoration(
